85e922d194b725e70fe9f3b4e45916ef77cd32f1
Bigtruck2 <bigburrito363@gmail.com>
2024-04-22 17:03:22 -0400

    remove spreads

--- src/scripts/paragraphLinks.ts
+++ src/scripts/paragraphLinks.ts
@@ -1,14 +1,14 @@
     const paragraphElements = document.querySelectorAll('p');
     let paragraphs = [...paragraphElements]
     let blockquotes = document.querySelectorAll('blockquote');
-    [...blockquotes].forEach((quoteElement)=>{
-            [...quoteElement.querySelectorAll('p')].forEach((quote)=>
-                {if(quote){
-                    paragraphs.splice(paragraphs.indexOf(quote),1)
-                }}
-            )
-            
-        });
+    blockquotes.forEach((quoteElement)=>{
+        quoteElement.querySelectorAll('p').forEach((quote)=>
+            {if(quote){
+                paragraphs.splice(paragraphs.indexOf(quote),1)
+            }}
+        )
+        
+    })
     paragraphs.forEach((element)=> {
         if(element.innerText.length>250 && element.outerHTML[1]!='<blockquote>'){ 
             element.outerHTML = `<div class='paragraph-wrapper'><a class='section-paragraph-link' href=#${element.id}></a><p class='' id=${element.id}>${element.innerHTML}</p>`
