158a791d73f102e7daa0488d31ae2fbe5ac58041
Bigtruck2 <bigburrito363@gmail.com>
2024-04-24 01:41:18 -0400

    fix bad random function

--- src/scripts/paragraphLinks.ts
+++ src/scripts/paragraphLinks.ts
@@ -8,9 +8,19 @@
             }}
         )
         
-    })
+    });
+    function hasDuplicates(array:string[]) {
+        return (new Set(array)).size !== array.length;
+    }
+    
+    //let id:string[] = []
     paragraphs.forEach((element)=> {
-        if(element.innerText.length>250 && element.outerHTML[1]!='<blockquote>'){ 
+        if(element.innerText.length>300 && element.parentElement?.tagName!='blockquote'){ 
+            //id.push(element.id);
             element.outerHTML = `<div class='paragraph-wrapper'><a class='section-paragraph-link' href=#${element.id}></a><p class='' id=${element.id}>${element.innerHTML}</p>`
         }
     });
+    //check for duplicated ids
+    /**if(hasDuplicates(id)){
+        console.error(id.filter((item, index) => index !== id.indexOf(item)))
+    }**/
